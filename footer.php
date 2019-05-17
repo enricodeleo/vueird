776ebc849ef63aa6f9afe5ff8983be7e202123bd
Enrico Deleo <hello@enricodeleo.com>
2019-05-17 14:19:34 +0200

    bugfix: closing div

--- footer.php
+++ footer.php
@@ -3,18 +3,18 @@
       		<div class="container text-center text-white">
       			<nav class="footer-navbar navbar navbar-dark">
       				<?php
-						wp_nav_menu( array(
+						wp_nav_menu(array(
 							'theme_location'  => 'footer_navigation',
 							'depth'           => 1,
 							'menu_class'      => 'nav m-auto small text-center',
 							'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
 							'walker'          => new WP_Bootstrap_Navwalker()
-						) );
-					?>
+						));
+						?>
       			</nav>
       			<div class="row">
       				<div class="col">
-      					<p><small>&copy; <?php echo bloginfo( 'name' ); ?> <?php echo date('Y'); ?> All Rights Reserved</small></p>
+      					<p><small>&copy; <?php echo bloginfo('name'); ?> <?php echo date('Y'); ?> All Rights Reserved</small></p>
       				</div>
       			</div>
 
@@ -27,15 +27,16 @@
 							'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
 							'walker'          => new WP_Bootstrap_Navwalker()
 						));
-					?>
+						?>
       			</nav>
       		</div>
       	</div>
 
       </footer>
 
-	  <?php wp_footer(); ?>
+      <?php wp_footer(); ?>
+	</div>
 
-	</body>
+</body>
 
 </html>
